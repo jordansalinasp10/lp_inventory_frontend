e879bad745096642c0516d02d2cb2ce347cdb025
Dario Laborde <dlaborde@espol.edu.ec>
2025-01-28 00:39:42 -0500

    delete folders and reformat

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,13 +3,10 @@ import 'package:flutter/material.dart';
 const colorList = <Color>[
   Colors.black,
   Colors.cyanAccent,
-  
-]
+];
+
 class AppTheme {
   final int SelectedColor;
 
-  AppTheme({
-    this.SelectedColor=0;});
-
-
+  AppTheme({this.SelectedColor=0});
 }
